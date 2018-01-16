522bea09390b402d3424ef67527c5636f55d5e44
AbellHype <35485344+AbellHype@users.noreply.github.com>
2018-01-16 01:15:06 -0800

    Created a program that displays 'Hello World'

---
+++ homework00/Greeter.java
@@ -0,0 +1,14 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Greeter.java
+ *  Purpose       :  Says "Hello world"
+ *  Author        :  Matthew Abell
+ *  Date          :  2018-01-16
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+public class Greeter {
+   public static void main( String args[] ) {
+      System.out.println( "Hello, world!" );
+   }
+}
