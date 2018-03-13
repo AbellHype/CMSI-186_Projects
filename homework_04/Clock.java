bc9f4ff4203b53e7c61c46e7f0bc75ba55b4b871
AbellHype <35485344+AbellHype@users.noreply.github.com>
2018-03-13 14:14:38 -0700

    Added Notes

--- homework_04/Clock.java
+++ homework_04/Clock.java
@@ -1,31 +1,28 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  File name     :  Clock.java
  *  Purpose       :  Provides a class defining methods for the ClockSolver class
- *  @author       :  B.J. Johnson
- *  Date written  :  2017-02-28
+ *  @author       :  Matthew Abell
+ *  Date written  :  2018-03-13
  *  Description   :  This class provides a bunch of methods which may be useful for the ClockSolver class
  *                   for Homework 4, part 1.  Includes the following:
  *
  *  Notes         :  None
  *  Warnings      :  None
- *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+ *  Exceptions    :  None needed (handled by ClockSolver)
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  Revision Histor
  *  ---------------
  *            Rev      Date     Modified by:  Reason for change/modification
  *           -----  ----------  ------------  -----------------------------------------------------------
  *  @version 1.0.0  2017-02-28  B.J. Johnson  Initial writing and release
+ *  @version 1.1.0  2018-03-13  Matthew Abell Working release
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 public class Clock {
   /**
    *  Class field definintions go here
    */
-   private static final double DEFAULT_TIME_SLICE_IN_SECONDS = 60.0;
-   private static final double INVALID_ARGUMENT_VALUE = -1.0;
    private static final double MAXIMUM_DEGREE_VALUE = 360.0;
-   private static final double HOUR_HAND_DEGREES_PER_SECOND = 0.00834;
-   private static final double MINUTE_HAND_DEGREES_PER_SECOND = 0.1;
    private double currentTime;
    private double angleDif;
    private double timeSlice;
@@ -47,7 +44,6 @@ public class Clock {
    *  Methods go here
    *
    *  Method to calculate the next tick from the time increment
-   *  @return double-precision value of the current clock tick
    */
    public void tick() {
       currentTime = currentTime + timeSlice;
@@ -55,9 +51,8 @@ public class Clock {
 
   /**
    *  Method to validate the angle argument
-   *  @param   argValue  String from the main programs args[0] input
-   *  @return  double-precision value of the argument
-   *  @throws  NumberFormatException
+   *  @param   argValue  Double from ClockSolver
+   *  @return  boolean: positive if the input is valid
    */
    public boolean validateAngleArg( double argValue ) {
       if (argValue > 0 && argValue < MAXIMUM_DEGREE_VALUE) {
@@ -72,14 +67,8 @@ public class Clock {
 
   /**
    *  Method to validate the optional time slice argument
-   *  @param  argValue  String from the main programs args[1] input
-   *  @return double-precision value of the argument or -1.0 if invalid
-   *  note: if the main program determines there IS no optional argument supplied,
-   *         I have elected to have it substitute the string "60.0" and call this
-   *         method anyhow.  That makes the main program code more uniform, but
-   *         this is a DESIGN DECISION, not a requirement!
-   *  note: remember that the time slice, if it is small will cause the simulation
-   *         to take a VERY LONG TIME to complete!
+   *  @param  argValue  Double from ClockSolver
+   *  @return boolean: positive if the input is valid
    */
    public boolean validateTimeSliceArg( double argValue ) {
       if (argValue > 0 && argValue < 1800.0) {
