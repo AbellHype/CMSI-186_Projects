e1b44c6843c2dca7cf7bc03f6c504c4ecf1ba970
AbellHype <35485344+AbellHype@users.noreply.github.com>
2018-03-30 13:40:54 -0400

    Finished Ball.java

--- homework_05/Ball.java
+++ homework_05/Ball.java
@@ -14,46 +14,96 @@
  *            Rev      Date     Modified by:  Reason for change/modification
  *           -----  ----------  ------------  -----------------------------------------------------------
  *  @version 1.0.0  2018-03-15  Matthew Abell Initial writing
+ *  @version 1.1.0  2018-03-27  Matthew Abell Final working version
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-public class Ball {
+import java.text.DecimalFormat;
 
+public class Ball {
+	
+    //Class field definintions
 	private double xpos;
 	private double ypos;
-	private double xvel;
-	private double yvel;
+	private double xvel; 
+	private double yvel; //all data stored in inches
+	private double timeslice;
 
-	public Ball (double xpos, double ypos, double xvel, double yvel){
+	//Constructor
+	public Ball (double xpos, double ypos, double xvel, double yvel, double timeslice){
 		this.xpos = xpos;
 		this.ypos = ypos;
 		this.xvel = xvel;
 		this.yvel = yvel;
+		this.timeslice = timeslice;
 	}
 
-	public void updatePos(double timeslice){
+	//Method to update the position of the ball
+	public void updatePos(){
 		xpos = xpos + (xvel * timeslice);
 		ypos = ypos + (yvel * timeslice);
 	}
 
-	public void updateVel(double timeslice){
-		double vel = Math.sqrt(xvel*xvel + yvel*yvel);
-		double ang = Math.atan(yvel/xvel);
-		if (vel >= 1){
-			vel = vel - (vel*.01*timeslice);
-			xvel = Math.cos(ang) * vel;
-			yvel = Math.sin(ang) * vel;
-		}
-		else {
+	/**
+    *  Method to return the position of the ball
+    *  @return  double array:array containing the x and y posistion of the ball
+    */
+	public double [] returnPos(){
+		double [] ans = {xpos,ypos};
+		return ans;
+	}
+
+	//Method to update the velocity of the ball
+	public void updateVel(){
+		if(Math.abs(xvel) > 1){
+			xvel = xvel - (xvel*.01*timeslice);
+		}	
+		else{
 			xvel = 0;
+		}
+		if(Math.abs(yvel) > 1){
+			yvel = yvel - (yvel*.01*timeslice);
+		}
+		else{
 			yvel = 0;
 		}	
 	}
 
+	/**
+    *  Method to check if the ball is moving
+    *  @return  boolean: positive if the ball is moving
+    */
+	public boolean isMoving(){
+		if (xvel == 0 && yvel == 0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+
+	/**
+    *  Method to check if the ball is touching another ball
+    *  @param   argValue  double array that contains the location of the other ball
+    *  @return  boolean: positive if the two ball are touching
+    */
+	public boolean collision(double [] Ball2){
+		double dist = Math.sqrt((xpos-Ball2[0])*(xpos-Ball2[0]) + (ypos-Ball2[1])*(ypos-Ball2[1]));
+		if (dist < 8.9){ //8.9 inches is the diameter of the soccer balls
+			return true;
+		}
+		return false;
+	}
+
+	/**
+    *  Method to create a string representation of the ball
+    *  @return  String: string containing information about the ball
+    */
 	public String toString(){
-		String xposstring = Double.toString(xpos/12);
-		String yposstring = Double.toString(ypos/12);
-		String xvelstring = Double.toString(xvel/12);
-		String yvelstring = Double.toString(yvel/12);
-		return ("There is a ball located at " + xposstring + "," + yposstring + ". The current velcity is " + xvelstring + "," + yvelstring + ".");
+		DecimalFormat numberFormat = new DecimalFormat("#.00");
+		String xposstring = (numberFormat.format(xpos/12)); 
+		String yposstring = (numberFormat.format(ypos/12));
+		String xvelstring = (numberFormat.format(xvel/12));
+		String yvelstring = (numberFormat.format(yvel/12)); // /12 is to convert inches to feet
+		return (" Position: " + xposstring + "," + yposstring + " Velocity: " + xvelstring + "," + yvelstring);
 	}
 }
