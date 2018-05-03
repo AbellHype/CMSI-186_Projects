a63fda459dfeda20bfa23acf6ad759d32f308c6a
AbellHype <35485344+AbellHype@users.noreply.github.com>
2018-05-03 10:16:02 -0700

    Added Notes

--- homework_07/DynamicChangeMaker.java
+++ homework_07/DynamicChangeMaker.java
@@ -1,10 +1,37 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  DynamicChangeMaker.java
+ * Purpose    :  Program that uses a matrix of tuples to find the optimal way to make change
+ * @author    :  Matthew Abell
+ * Date       :  2018-05-03
+ * Description:  This program creates a matrix out of tuples to use dynamic programming to find the optimal
+ *               way to make a certain amount of change when given a list of coins with various denominations
+ * Notes      :  None
+ * Warnings   :  None
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Revision History
+ * ================
+ *   Ver      Date     Modified by:  Reason for change or modification
+ *  -----  ----------  ------------  ---------------------------------------------------------------------
+ *  1.0.0  2018-05-03  Matthew Abell Initial release
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 import java.util.Arrays;
 
 public class DynamicChangeMaker{
 
 	public DynamicChangeMaker() {
+		//nothing needed in the constructor
 	}
 
+  /**
+   * This method finds a tuple represenation of the optimal way to make change
+   *
+   * @param coins:an int array that lists all possible coins, amount: the integer for
+   * which the program is making change for
+   *
+   * @return a tuple that represents the optimal number of each coin that makes the proper
+   * amount of change
+   */
 	public static Tuple makeChangeWithDynamicProgramming(int [] coins, int amount){
 		if(isInvalid(coins,amount)){
 			return new Tuple(new int[0]);
@@ -44,6 +71,15 @@ public class DynamicChangeMaker{
 		return deSort(solver[coins.length-1][amount-1],sortMem);
 	}
 
+  /**
+   * This method finds a tuple represenation of the optimal way to make change
+   *
+   * @param coins:an int array that lists all possible coins, amount: the integer for
+   * which the program is making change for
+   *
+   * @return a boolean that is true if the arguements for makeChangeWithDynamicProgamming 
+   * are invalid
+   */
 	public static boolean isInvalid(int [] coins, int amount){
 		if (amount < 0){
 			System.out.println("Bad input, please input a positive target amount of money");
@@ -58,6 +94,13 @@ public class DynamicChangeMaker{
 		return false;
 	}
 
+  /**
+   * This method creates an int array that 'remembers' the order of the coins
+   *
+   * @param coins:a int array that lists all possible coins
+   *
+   * @return an int array that holds the new location of the coins when they get sorted
+   */
 	public static int [] sorter(int [] coins){
 		int [] ans = new int [coins.length];
 		int [] sortCoins = coins.clone();
@@ -68,6 +111,13 @@ public class DynamicChangeMaker{
 		return ans;
 	}
 
+  /**
+   * This method unsorts a previously sorted tuple
+   *
+   * @param tup: a tuple that is being unsorted, mem: an integer array that contains the 
+   * previous locations of the values in the tuple
+   * @return the inputted tuple unsorted back to its original form
+   */
 	public static Tuple deSort(Tuple tup, int [] mem){
 		Tuple ans = new Tuple(tup.length());
 		for (int i = 0; i < tup.length(); i++){
@@ -76,6 +126,14 @@ public class DynamicChangeMaker{
 		return ans;
 	}
 
+  /**
+   * This method finds the index of an arguement in an array
+   *
+   * @param arr: an integer array that is being searched, ind: the integer value that
+   * is being searched for
+   * @return an integer that is the index where the arguement is located in the array,
+   * returns -1 if arguement isn't in the array
+   */
 	public static int indexOf(int [] arr, int ind){
 		int ans = -1;
 		for (int i = 0; i < arr.length; i++){
@@ -86,6 +144,13 @@ public class DynamicChangeMaker{
 		return ans;
 	}
 
+  /**
+   * This method finds the number of coins in a tuple
+   *
+   * @param tup: the tuple in which we are counting the number of coins
+   *
+   * @return an integer that cointains the number of coins in the inputted tuple
+   */
 	public static int numOfCoins(Tuple tup){
 		int ans = 0;
 		for (int i = 0; i < tup.length(); i++){
@@ -97,4 +162,5 @@ public class DynamicChangeMaker{
 	public static void main(String [] args){
 		;
 	}
+
 }
